2946f19eae168e3b8d1b1a597227d4c26adef66a
prcv <prcv@cisco.com>
2020-11-12 07:31:15 +0530

    updated changes

--- src/Product.js
+++ src/Product.js
@@ -28,31 +28,31 @@ function Product({ id, title, image, price, rating }) {
     return (
 
 
-       
-            <div className="product">
-                <div className="product__info">
-                    <p>{title}</p>
-                    <p className="product__price">
-                        <small> $</small>
-                        <strong>{price}</strong>
-                    </p>
-                    <div className="product__rating">
-                        {Array(rating).fill().map((_, i) => (
-                            <p key={i}>🌟</p>
-                        ))}
-                    </div>
-                </div>
-                <img src={image} alt="" />
-                <animated.div
-                        className="card"
-                        onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
-                        onMouseLeave={() => set({ xys: [0, 0, 1] })}
-                        style={{ transform: props.xys.interpolate(trans) }} >
-                <button onClick={addToBasket}>Add to Basket</button>
-                </animated.div >
 
+        <div className="product">
+            <div className="product__info">
+                <p>{title}</p>
+                <p className="product__price">
+                    <small> $</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="product__rating">
+                    {Array(rating).fill().map((_, i) => (
+                        <p key={i}>🌟</p>
+                    ))}
+                </div>
             </div>
-      
+            <img src={image} alt="" />
+            <animated.div
+                className="card"
+                onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+                onMouseLeave={() => set({ xys: [0, 0, 1] })}
+                style={{ transform: props.xys.interpolate(trans) }} >
+                <button onClick={addToBasket}>Add to Basket</button>
+            </animated.div >
+
+        </div>
+
 
 
 
